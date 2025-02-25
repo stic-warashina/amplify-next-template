51b0ab7f9ee7e81019083fc64a5c8a11afb52fcc
STICLAN\warashina <warashina@netinsz.co.jp>
2025-02-25 16:31:04 +0900

    add storage browser

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { storage } from './storage/resource.js';
 
 defineBackend({
   auth,
-  data,
+  storage,
 });
