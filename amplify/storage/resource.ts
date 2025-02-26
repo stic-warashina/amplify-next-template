515a0ac55bf86067217e75eabb03a0b646d7a8fa
STICLAN\warashina <warashina@netinsz.co.jp>
2025-02-26 10:58:26 +0900

    正確なストレージ名を記述

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,7 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-    name: 'myProjectFiles',
+    name: 'test-ragchat-warashina-test ',
     access: (allow) => ({
       'public/*': [
         allow.guest.to(['read']),
