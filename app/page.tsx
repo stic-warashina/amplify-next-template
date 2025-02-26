95bc5dd028a5a3cdb78862a9f95f7f76a258d0bb
STICLAN\warashina <warashina@netinsz.co.jp>
2025-02-26 11:23:43 +0900

    packageの依存関係の見直し

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "@aws-amplify/ui-react/styles.css";
-import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
 import outputs from "@/amplify_outputs.json";
 import { Authenticator, Button } from "@aws-amplify/ui-react";
 import { StorageBrowser } from "@aws-amplify/ui-react-storage";
